76b451884d9130345459b2bc0c5e8c83162e12f6
Dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2022-04-02 00:35:55 -0400

    remove newlines

--- setup.sh
+++ setup.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-pacmans -S efibootmgr \ 
-grub \ 
-networkmanager \ 
-man \ 
-tmux \ 
-openssh \ 
-xorg-server \ 
-xorg-xinit \ 
-xfce4 \ 
-xfce4-goodies \ 
-lightdm \ 
-lightdm-gtk-greeter
+pacmans -S efibootmgr \ grub \ networkmanager \ man \ tmux \ openssh \ xorg-server \ xorg-xinit \ xfce4 \ xfce4-goodies \ lightdm \ lightdm-gtk-greeter
